4e0d402455caa464ddf063debbe67b6f31df9a2a
Praenyth <praenyth@gmail.com>
2022-10-30 20:15:15 -0400

    ok massiver update made minecart going on upwards and diagonal rails actually work

---
+++ src/main/java/me/praenyth/plugins/minecartcollisions/Utils.java
@@ -0,0 +1,241 @@
+package me.praenyth.plugins.minecartcollisions;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Minecart;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Vehicle;
+import org.bukkit.entity.minecart.RideableMinecart;
+import org.bukkit.event.vehicle.VehicleMoveEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.material.PoweredRail;
+import org.bukkit.material.Rails;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
+
+import java.util.List;
+
+public class Utils {
+
+    // a bunch of shamelessly "borrowed" code
+    public static void damagePlayer(Player p, double damage, Entity source) {
+        double points = p.getAttribute(Attribute.GENERIC_ARMOR).getValue();
+        double toughness = p.getAttribute(Attribute.GENERIC_ARMOR_TOUGHNESS).getValue();
+        PotionEffect effect = p.getPotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
+        int resistance = effect == null ? 0 : effect.getAmplifier();
+        int epf = getEPF(p.getInventory());
+
+        p.damage(calculateDamageApplied(damage, points, toughness, resistance, epf), source);
+    }
+
+    private static double calculateDamageApplied(double damage, double points, double toughness, int resistance, int epf) {
+        double withArmorAndToughness = damage * (1 - Math.min(20, Math.max(points / 5, points - damage / (2 + toughness / 4))) / 25);
+        double withResistance = withArmorAndToughness * (1 - (resistance * 0.2));
+        double withEnchants = withResistance * (1 - (Math.min(20.0, epf) / 25));
+        return withEnchants;
+    }
+
+    private static int getEPF(PlayerInventory inv) {
+        ItemStack helm = inv.getHelmet();
+        ItemStack chest = inv.getChestplate();
+        ItemStack legs = inv.getLeggings();
+        ItemStack boot = inv.getBoots();
+
+        return (helm != null ? helm.getEnchantmentLevel(Enchantment.DAMAGE_ALL) : 0) +
+                (chest != null ? chest.getEnchantmentLevel(Enchantment.DAMAGE_ALL) : 0) +
+                (legs != null ? legs.getEnchantmentLevel(Enchantment.DAMAGE_ALL) : 0) +
+                (boot != null ? boot.getEnchantmentLevel(Enchantment.DAMAGE_ALL) : 0);
+    }
+
+    public static RideableMinecart getValidMineCart(Vehicle vehicle, boolean mustHavePassenger)
+    {
+        RideableMinecart cart = null;
+
+        if (!(vehicle instanceof RideableMinecart))
+            return null;
+
+        cart = (RideableMinecart) vehicle;
+
+        Entity firstPassenger = GetFirstPassenger(cart);
+        if (firstPassenger == null)
+            return null;
+
+        if (mustHavePassenger && (cart.isEmpty() || !(firstPassenger instanceof Player)))
+            return null;
+
+        return cart;
+    }
+
+    public static Entity GetFirstPassenger(Minecart toCart)
+    {
+        List<Entity> passengers = toCart.getPassengers();
+
+        if (passengers.isEmpty())
+            return null;
+
+        return passengers.get(0);
+    }
+
+    public static boolean isFlatRail(Location location)
+    {
+        if (location.getBlock().getType() == Material.RAIL)
+        {
+            Rails testRail = (Rails) location.getBlock().getState().getData();
+            if (!testRail.isOnSlope())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isRailPerpendicular(Location myLocation, Location otherLocation)
+    {
+        Block myBlock = myLocation.getBlock();
+        Block otherBlock = otherLocation.getBlock();
+        if (otherBlock.getType() == Material.RAIL)
+        {
+            if (myBlock.getData() == (byte) 0 && otherBlock.getData() == (byte) 1)
+            {
+                return true;
+            } else if (myBlock.getData() == (byte) 1 && otherBlock.getData() == (byte) 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isRailParallel(Location myLocation, Location otherLocation)
+    {
+        Block myBlock = myLocation.getBlock();
+        Block otherBlock = otherLocation.getBlock();
+        if (otherBlock.getType() == Material.RAIL)
+        {
+            if (myBlock.getData() == otherBlock.getData())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Vector getUnitVectorFromYaw(float yaw)
+    {
+        BlockFace facing = getBlockFaceFromYaw(yaw);
+        switch (facing)
+        {
+            case SOUTH:
+                return new Vector(0, 0, 1);
+            case WEST:
+                return new Vector(-1, 0, 0);
+            case NORTH:
+                return new Vector(0, 0, -1);
+            default: // EAST
+                return new Vector(1, 0, 0);
+        }
+    }
+
+
+    public static boolean isIntersection(Location myLocation, Vector movementDirection)
+    {
+        if (Utils.isFlatRail(myLocation))
+        {
+            // Search for intersection
+            Location front = myLocation.clone().add(movementDirection.normalize());
+            Location back = myLocation.clone().subtract(movementDirection.normalize());
+            Location left = myLocation.clone().add(movementDirection.getZ(), 0, -movementDirection.getX()); // go one left
+            Location right = myLocation.clone().add(-movementDirection.getZ(), 0, movementDirection.getX()); // go one right
+
+            if (Utils.isRailPerpendicular(myLocation, left) && Utils.isRailPerpendicular(myLocation, right))
+            {
+                return true;
+            } else if ((Utils.isRailPerpendicular(myLocation, left)
+                    && (Utils.isRailParallel(myLocation, front) || Utils.isRailParallel(myLocation, back)))
+                    || (Utils.isRailPerpendicular(myLocation, right)
+                    && (Utils.isRailParallel(myLocation, front) || Utils.isRailParallel(myLocation, back))))
+            {
+                return true;
+            } else if ((Utils.isRailParallel(myLocation, left)
+                    && (Utils.isRailPerpendicular(myLocation, front) || Utils.isRailPerpendicular(myLocation, back)))
+                    || (Utils.isRailParallel(myLocation, right)
+                    && (Utils.isRailPerpendicular(myLocation, front) || Utils.isRailPerpendicular(myLocation, back))))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static BlockFace getBlockFaceFromYaw(float yaw)
+    {
+
+        if (yaw < 0)
+        { // Map all negative values to positives. E.g. -45 degree = +315 degree
+            yaw = yaw + 360;
+        }
+        yaw = yaw % 360; // crop value, e.g. if it's 460 degree --> 100 degree
+
+        float straightAngle = 90;
+
+        if ((yaw >= 0) && (yaw < (straightAngle / 2)) || (yaw >= (360 - (straightAngle / 2))))
+        {
+            return BlockFace.SOUTH;
+        } else if ((yaw >= (straightAngle / 2)) && (yaw < 135))
+        {
+            return BlockFace.WEST;
+        } else if ((yaw >= 135) && (yaw < (360 - (straightAngle * 1.5))))
+        {
+            return BlockFace.NORTH;
+        } else
+        {
+            return BlockFace.EAST;
+        }
+    }
+
+    public static boolean isMovingUp(VehicleMoveEvent event)
+    {
+        return event.getTo().getY() - event.getFrom().getY() > 0;
+    }
+
+    public static boolean isMovingDown(VehicleMoveEvent event)
+    {
+        return event.getTo().getY() - event.getFrom().getY() < 0;
+    }
+
+    public static Rails getRailInFront(Location testLoc)
+    {
+        try
+        {
+            // Slopes that go down/fall have the blocks underneath the current y-level
+            Location testLocUnder = testLoc.clone().subtract(0, 1, 0);
+
+            if (testLoc.getBlock().getType() == Material.RAIL)
+            {
+                // Detects rising slope
+                return (Rails) testLoc.getBlock().getState().getData();
+            } else if (testLocUnder.getBlock().getType() == Material.RAIL)
+            {
+                // Detects falling slope
+                return (Rails) testLocUnder.getBlock().getState().getData();
+            } else if (testLoc.getBlock().getType() == Material.POWERED_RAIL)
+            {
+                return (PoweredRail) testLoc.getBlock().getState().getData();
+            } else if (testLocUnder.getBlock().getType() == Material.POWERED_RAIL)
+            {
+                return (PoweredRail) testLocUnder.getBlock().getState().getData();
+            }
+        } catch (ClassCastException e)
+        {
+            // no valid rail found
+        }
+        return null;
+    }
+}
--- src/main/java/me/praenyth/plugins/minecartcollisions/events/CollisionListener.java
+++ src/main/java/me/praenyth/plugins/minecartcollisions/events/CollisionListener.java
@@ -1,70 +1,41 @@
 package me.praenyth.plugins.minecartcollisions.events;
 
-import org.bukkit.Bukkit;
+import me.praenyth.plugins.minecartcollisions.Utils;
+import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.block.Block;
-import org.bukkit.block.data.Rail;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.*;
-import org.bukkit.entity.minecart.RideableMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
+import org.bukkit.material.Rails;
 import org.bukkit.util.Vector;
-
-import java.util.Objects;
 
 public class CollisionListener implements Listener {
 
     @EventHandler
     public void onVehicleMove(VehicleMoveEvent e) {
-        Vehicle vehicle = e.getVehicle();
+
+        float MAX_SPEED = 20f;
+        float MAX_SPEED_INTERSECTION = 1f;
 
         Minecart cart = null;
 
-
-        if (vehicle instanceof Minecart) {
-            cart = (Minecart) vehicle;
+        if (e.getVehicle() instanceof Minecart) {
+            cart = (Minecart) e.getVehicle();
 
             // set max speed of the minecart depending on item
-            float tempSpeed = 20f;
+            float tempSpeed = MAX_SPEED;
             if (cart.getPassengers().size() >= 1) {
-                if (vehicle.getPassengers().get(0) instanceof Player) {
-                    Player riding = (Player) vehicle.getPassengers().get(0);
-                    if (riding.getInventory().getItemInOffHand().getType().equals(Material.FEATHER) || riding.getInventory().getItemInMainHand().getType().equals(Material.FEATHER)) {
-                        tempSpeed = 100f;
-                    } else if (riding.getInventory().getItemInOffHand().getType().equals(Material.PUFFERFISH_BUCKET) || riding.getInventory().getItemInMainHand().getType().equals(Material.PUFFERFISH_BUCKET)) {
-                        tempSpeed = 1f;
-                    }
-                }
-            }
+                if (cart.getPassengers().get(0) instanceof Player) {
 
-            // set velocity depending on rail direction
-            if (cart.getLocation().getBlock().getType().equals(Material.RAIL)) {
-                Block rail = cart.getLocation().getBlock();
-                if (rail.getBlockData() instanceof Rail) {
-                    Rail rail2 = (Rail) rail.getBlockData();
-                    switch (rail2.getShape()) {
-                        case ASCENDING_SOUTH:
-                            cart.setVelocity(new Vector(0.0, 0.0, cart.getVelocity().getZ()*-1));
-                            cart.setFlyingVelocityMod(new Vector(0.0, 0.0, cart.getVelocity().getZ()*-1));
-                            break;
-                        case ASCENDING_NORTH:
-                            cart.setVelocity(new Vector(0.0, 0.0, cart.getVelocity().getZ()));
-                            cart.setFlyingVelocityMod(new Vector(0.0, 0.0, cart.getVelocity().getZ()));
-                            break;
-                        case ASCENDING_WEST:
-                            cart.setVelocity(new Vector(cart.getVelocity().getX(), 0.0, 0.0));
-                            cart.setFlyingVelocityMod(new Vector(cart.getVelocity().getX(), 0.0, 0.0));
-                            break;
-                        case ASCENDING_EAST:
-                            cart.setVelocity(new Vector(cart.getVelocity().getX()*-1, 0.0, 0.0));
-                            cart.setFlyingVelocityMod(new Vector(cart.getVelocity().getX()*-1, 0.0, 0.0));
+                    Player riding = (Player) cart.getPassengers().get(0);
+                    Material mainHandItemType = riding.getInventory().getItemInMainHand().getType();
+                    Material offHandItemType = riding.getInventory().getItemInOffHand().getType();
+
+                    if (offHandItemType.equals(Material.FEATHER) || mainHandItemType.equals(Material.FEATHER)) {
+                        tempSpeed = 100f;
+                    } else if (offHandItemType.equals(Material.PUFFERFISH_BUCKET) || mainHandItemType.equals(Material.PUFFERFISH_BUCKET)) {
+                        tempSpeed = 1f;
                     }
                 }
             }
@@ -72,75 +43,140 @@ public class CollisionListener implements Listener {
             cart.setMaxSpeed(tempSpeed);
         }
 
+        // minecart movement
+        if (cart != null) {
+            Location cartLocation = cart.getLocation();
+            Vector cartVelocity = cart.getVelocity();
+
+            Location locationInFront = cartLocation.clone();
+            Vector cartDirection = cartVelocity.clone().normalize();
+            Rails railUnderCart;
+            try
+            {
+                railUnderCart = (Rails) cart.getLocation().getBlock().getState().getData();
+            } catch (ClassCastException exception)
+            {
+                return;
+            }
+            for (int i = 1; i < 3; i++) // checks 3 blocks ahead
+            {
+                locationInFront.add(cartDirection.multiply(i));
+                Rails railInFront = Utils.getRailInFront(locationInFront);
+
+                if (railInFront != null)
+                {
+
+                    if (railInFront.isCurve() || railInFront.isOnSlope())
+                    {
+                        if (railUnderCart.isOnSlope() && Utils.isMovingDown(e))
+                        {
+                            // Don't do anything if we are on a downward slope
+                            return;
+                        } else
+                        {
+                            cart.setMaxSpeed(MAX_SPEED_INTERSECTION);
+                            return;
+                        }
+                    } else if ((cartVelocity.length() > MAX_SPEED_INTERSECTION)
+                            && Utils.isIntersection(locationInFront, cartDirection))
+                    {
+                        cart.setMaxSpeed(MAX_SPEED_INTERSECTION);
+                        return;
+                    }
+                }
+            }
+        }
+
+
+
         // minecart damage mechanic
         if (cart != null) {
-            for (Entity entity : cart.getNearbyEntities(1.5, -1,1.5)) {
+            String hitSound = "prae.danielsmp.minecart";
+            Vector cartVelocity = cart.getVelocity();
+            Location cartLocation = cart.getLocation();
+
+            for (Entity entity : cart.getNearbyEntities(1.5, 1,1.5)) {
                 if (!cart.getPassengers().contains(entity)) {
                     if (entity instanceof LivingEntity) {
 
                         LivingEntity hitEntity = (LivingEntity) entity;
+                        Vector hitEntityVelocity = hitEntity.getVelocity();
+                        Location hitEntityLocation = hitEntity.getLocation();
 
                         if (cart.getVelocity().length() > 1) {
-
                             if (cart.getPassengers().size() >= 1) {
                                 if (hitEntity instanceof Player) {
+
+                                    Player hitPlayer = (Player) hitEntity;
+
                                     int tempDamage = 12;
-                                    if (!((Player)hitEntity).isFlying()) {
-                                        hitEntity.setVelocity(cart.getVelocity().subtract(hitEntity.getVelocity().normalize()).setY(1));
-                                        if (vehicle.getPassengers().get(0) instanceof Player) {
-                                            Player riding = (Player) vehicle.getPassengers().get(0);
-                                            if (riding.getInventory().getItemInOffHand().getType().equals(Material.TNT) || riding.getInventory().getItemInMainHand().getType().equals(Material.TNT)) {
+                                    if (!hitPlayer.isFlying()) {
+                                        hitPlayer.setVelocity(cartVelocity.subtract(hitEntityVelocity.normalize()).setY(cartVelocity.length()));
+                                        if (cart.getPassengers().get(0) instanceof Player) {
+
+                                            Player riding = (Player) cart.getPassengers().get(0);
+                                            Material offHandItemType = riding.getInventory().getItemInOffHand().getType();
+                                            Material mainHandItemType = riding.getInventory().getItemInMainHand().getType();
+
+                                            if (offHandItemType.equals(Material.TNT) || mainHandItemType.equals(Material.TNT)) {
                                                 riding.getWorld().createExplosion(riding.getLocation(), 3f);
-                                            } else if (riding.getInventory().getItemInOffHand().getType().equals(Material.PUFFERFISH_BUCKET) || riding.getInventory().getItemInMainHand().getType().equals(Material.PUFFERFISH_BUCKET)) {
+                                            } else if (offHandItemType.equals(Material.PUFFERFISH_BUCKET) || mainHandItemType.equals(Material.PUFFERFISH_BUCKET)) {
                                                 tempDamage = 24;
-                                            } else if (riding.getInventory().getItemInOffHand().getType().equals(Material.FEATHER) || riding.getInventory().getItemInMainHand().getType().equals(Material.FEATHER)) {
+                                            } else if (offHandItemType.equals(Material.FEATHER) || mainHandItemType.equals(Material.FEATHER)) {
                                                 tempDamage = 0;
-                                            } else if (riding.getInventory().getItemInMainHand().getType().equals(Material.ANVIL) || riding.getInventory().getItemInOffHand().getType().equals(Material.ANVIL)) {
+                                            } else if (offHandItemType.equals(Material.ANVIL) || mainHandItemType.equals(Material.ANVIL)) {
                                                 tempDamage = 2000;
                                             }
                                         }
                                     }
-                                    damagePlayer(((Player)hitEntity), tempDamage, cart);
+                                    Utils.damagePlayer(((Player)hitEntity), tempDamage, cart);
 
-                                    hitEntity.getWorld().playSound(hitEntity.getLocation(), "prae.danielsmp.minecart", 1f, 1f);
+                                    hitEntity.getWorld().playSound(hitEntityLocation, hitSound, 1f, 1f);
 
                                 } else {
                                     if (!(hitEntity instanceof Item)) {
                                         int tempDamage = 12;
-                                        Vector tempVelocity = cart.getVelocity().subtract(hitEntity.getVelocity().normalize());
-                                        if (vehicle.getPassengers().get(0) instanceof Player) {
-                                            Player riding = (Player) vehicle.getPassengers().get(0);
-                                            if (riding.getInventory().getItemInOffHand().getType().equals(Material.TNT) || riding.getInventory().getItemInMainHand().getType().equals(Material.TNT)) {
+                                        Vector tempVelocity = cartVelocity.subtract(hitEntityVelocity.normalize());
+                                        if (cart.getPassengers().get(0) instanceof Player) {
+
+                                            Player riding = (Player) cart.getPassengers().get(0);
+                                            Material offHandItemType = riding.getInventory().getItemInOffHand().getType();
+                                            Material mainHandItemType = riding.getInventory().getItemInMainHand().getType();
+
+                                            if (offHandItemType.equals(Material.TNT) || mainHandItemType.equals(Material.TNT)) {
                                                 riding.getWorld().createExplosion(riding.getLocation(), 3f);
-                                            } else if (riding.getInventory().getItemInOffHand().getType().equals(Material.PUFFERFISH_BUCKET) || riding.getInventory().getItemInMainHand().getType().equals(Material.PUFFERFISH_BUCKET)) {
+                                            } else if (offHandItemType.equals(Material.PUFFERFISH_BUCKET) || mainHandItemType.equals(Material.PUFFERFISH_BUCKET)) {
                                                 tempDamage = 24;
-                                            } else if (riding.getInventory().getItemInOffHand().getType().equals(Material.FEATHER) || riding.getInventory().getItemInMainHand().getType().equals(Material.FEATHER)) {
+                                            } else if (offHandItemType.equals(Material.FEATHER) || mainHandItemType.equals(Material.FEATHER)) {
                                                 tempDamage = 0;
-                                                tempVelocity = cart.getVelocity().setY(cart.getVelocity().getY()+1);
-                                            } else if (riding.getInventory().getItemInMainHand().getType().equals(Material.ANVIL) || riding.getInventory().getItemInOffHand().getType().equals(Material.ANVIL)) {
+                                                tempVelocity = cart.getVelocity().setY(cart.getVelocity().length());
+                                            } else if (offHandItemType.equals(Material.ANVIL) || mainHandItemType.equals(Material.ANVIL)) {
                                                 tempDamage = 2000;
                                             }
                                         }
                                         hitEntity.damage(tempDamage);
                                         hitEntity.setVelocity(tempVelocity);
 
-                                        hitEntity.getWorld().playSound(hitEntity.getLocation(), "prae.danielsmp.minecart", 1f, 1f);
+                                        hitEntity.getWorld().playSound(hitEntityLocation, hitSound, 1f, 1f);
                                     }
                                 }
                             } else {
                                 if (hitEntity instanceof Player) {
-                                    if (!((Player)hitEntity).isFlying()) {
-                                        hitEntity.setVelocity(cart.getVelocity().subtract(hitEntity.getVelocity().normalize()).setY(1));
-                                    }
-                                    damagePlayer(((Player)hitEntity), 6, cart);
 
-                                    hitEntity.getWorld().playSound(hitEntity.getLocation(), "prae.danielsmp.minecart", 1f, 1f);
+                                    Player hitPlayer = (Player) hitEntity;
+
+                                    if (!hitPlayer.isFlying()) {
+                                        hitPlayer.setVelocity(cartVelocity.subtract(hitEntityVelocity.normalize()).setY(cartVelocity.length()));
+                                    }
+                                    Utils.damagePlayer(hitPlayer, 6, cart);
+
+                                    hitPlayer.getWorld().playSound(hitEntityLocation, hitSound, 1f, 1f);
                                 } else {
                                     if (!(hitEntity instanceof Item)) {
                                         hitEntity.damage(6);
-                                        hitEntity.setVelocity(cart.getVelocity().subtract(hitEntity.getVelocity().normalize()).setY(1));
+                                        hitEntity.setVelocity(cartVelocity.subtract(hitEntityVelocity.normalize()).setY(cartVelocity.length()));
 
-                                        hitEntity.getWorld().playSound(hitEntity.getLocation(), "prae.danielsmp.minecart", 1f, 1f);
+                                        hitEntity.getWorld().playSound(hitEntityLocation, hitSound, 1f, 1f);
                                     }
                                 }
                             }
@@ -150,34 +186,5 @@ public class CollisionListener implements Listener {
                 }
             }
         }
-    }
-
-    private void damagePlayer(Player p, double damage, Entity source) {
-        double points = p.getAttribute(Attribute.GENERIC_ARMOR).getValue();
-        double toughness = p.getAttribute(Attribute.GENERIC_ARMOR_TOUGHNESS).getValue();
-        PotionEffect effect = p.getPotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
-        int resistance = effect == null ? 0 : effect.getAmplifier();
-        int epf = getEPF(p.getInventory());
-
-        p.damage(calculateDamageApplied(damage, points, toughness, resistance, epf), source);
-    }
-
-    private double calculateDamageApplied(double damage, double points, double toughness, int resistance, int epf) {
-        double withArmorAndToughness = damage * (1 - Math.min(20, Math.max(points / 5, points - damage / (2 + toughness / 4))) / 25);
-        double withResistance = withArmorAndToughness * (1 - (resistance * 0.2));
-        double withEnchants = withResistance * (1 - (Math.min(20.0, epf) / 25));
-        return withEnchants;
-    }
-
-    private static int getEPF(PlayerInventory inv) {
-        ItemStack helm = inv.getHelmet();
-        ItemStack chest = inv.getChestplate();
-        ItemStack legs = inv.getLeggings();
-        ItemStack boot = inv.getBoots();
-
-        return (helm != null ? helm.getEnchantmentLevel(Enchantment.DAMAGE_ALL) : 0) +
-                (chest != null ? chest.getEnchantmentLevel(Enchantment.DAMAGE_ALL) : 0) +
-                (legs != null ? legs.getEnchantmentLevel(Enchantment.DAMAGE_ALL) : 0) +
-                (boot != null ? boot.getEnchantmentLevel(Enchantment.DAMAGE_ALL) : 0);
     }
 }
